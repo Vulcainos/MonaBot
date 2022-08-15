394a8e7f28befdaf38397fc742bdbb010c6d0582
Vulcainos <Vulcainos@users.noreply.github.com>
2022-08-15 03:47:12 +0200

    Add humidity, indentation and  more

--- discordbot/index.js
+++ discordbot/index.js
@@ -42,6 +42,7 @@ async function getArticles() {
 
 /**
  * Set the currencies values for USD, GBP, JPY and EUR.
+ * Set the crypto values for BTC and DOGE.
  * Default values are set to "EUR".
  * @param {*} currency_id 
  */
@@ -69,8 +70,7 @@ function setValueOfCurrenciesFrom(currency_id = "EUR") {
         cryptoValues = [];
         listCoin.forEach(coin => {
             cryptoValues.push({"NAME": coin.name, "PRICE" : coin.current_price, "HIGH" : coin.high_24h, "LOW" : coin.low_24h, "PERCENTAGE" :coin.market_cap_change_percentage_24h});
-            //console.log(`name : ${coin.name}, price : ${coin.current_price}, high_24h : ${coin.high_24h}, low_24h : ${coin.low_24h}, market_cap_change_percentage_24h : ${coin.market_cap_change_percentage_24h}`);
-        });
+        } );
     })
     .catch(function(error) {
         console.log(`Cant fetch crypto values from coingecko api : ${error.message}`);
@@ -105,7 +105,6 @@ client.once('ready', () => {
 
     // Loop for detect time and send daily journal to users.
     setInterval(() => {
-
         var date = new Date(); //Get current date.
         date.setHours(date.getHours() + 2); //Add 2 hours to current date to get UTC+2 date.
 
@@ -126,122 +125,9 @@ client.once('ready', () => {
     }, 61000) // checks every minute and 10secs (61000 ms)
 });
 
-client.on('messageCreate', async message => {
-    console.log(message.content);
-    if(message.content.startsWith('!meteo')) {
-        if(message.content.replace("!meteo", "") == "") {
-            await message.reply("Désolé, mais ce que vous avez envoyé n'est pas un nombre valide.")
-            return;
-
-        }
-        try {
-            var zipcode_depart = parseInt(message.content.replace("!meteo", ""));
-            console.log(zipcode_depart);
-        } catch (error) {
-            await message.reply("Désolé, mais ce que vous avez envoyé n'est pas un nombre valide.")
-            return;
-        }
-        if(zipcode_depart == NaN) {
-            await message.reply("Désolé, mais ce que vous avez envoyé n'est pas un nombre valide.")
-            return;
-        }
-        if(zipcode_depart == undefined) {
-            await message.reply("Désolé, mais ce que vous avez envoyé n'est pas un nombre valide.")
-
-        }
-        if(userCooldown[message.author.id] != undefined) {
-            if(userCooldown[message.author.id] > Date.now()) {
-                await message.reply("Veuillez attendre une minute avant de recommencer.")
-                return;
-            } else {
-                delete userCooldown[message.author.id];
-            }
-        } else {
-            userCooldown[message.author.id] = Date.now()+60000;
-        }
-        var temp = "pas de température trouvée..."
-        var depart_name = departements[zipcode_depart];
-        if(zipcode_depart >= 971 && zipcode_depart <= 976) {   
-            depart_name = "Outre mer"
-        }
-        if(zipcode_depart < 0 || (zipcode_depart > 95 && zipcode_depart < 971) || zipcode_depart > 976) {
-            await message.reply("Ce département n'existe pas");
-            return;
-        }
-        // Weather embed 1
-        var WeatherEmbed;
-        //first get the biggest city in the departement
-        getBiggestCityOnDepartment(zipcode_depart).then(cityInfo => {
-            //get current weather
-            getWeatherDataFrom(cityInfo.codesPostaux[0]).then(weather => {
-                var emojiState = ":question:";
-                var stateText;
-                if(weather.state == "Clouds") {
-                    emojiState = ":cloud:";
-                    weather.desc = "nuageux";
-                }
-                if(weather.state == "Clear") {
-                    emojiState = ":sun_with_face:"
-                    weather.desc = "ensoleillé"
-                }
-                if(weather.state == "Mist") {
-                    emojiState = ":white_sun_small_cloud:"
-                    weather.desc = "brumeux";
-                }
-                if(weather.state == "Rain") {
-                    emojiState = ":cloud_rain:";
-                    weather.desc = "pluvieux";
-                }
-
-                getForecastDataFrom(cityInfo.codesPostaux[0]).then(forecast => {
-                    getRandomFieldsPhoto().then(photoURL => {
-
-                        var emojiStateForecast = ":question:";
-                    var stateText;
-                    if(forecast.state == "Clouds") {
-                        emojiStateForecast = ":cloud:";
-                        forecast.desc = "nuageux";
-                    }
-                    if(forecast.state == "Clear") {
-                        emojiStateForecast = ":sun_with_face:"
-                        forecast.desc = "ensoleillé"
-                    }
-                    if(forecast.state == "Mist") {
-                        emojiStateForecast = ":white_sun_small_cloud:"
-                        forecast.desc = "brumeux";
-                    }
-                    if(forecast.state == "Rain") {
-                        emojiStateForecast = ":cloud_rain:";
-                        forecast.desc = "pluvieux";
-                    }
-                    WeatherEmbed = new MessageEmbed()
-                    .setColor('#0099ff')
-                    .setTitle('Mona Météo :satellite:')
-                    .setAuthor({ name: 'MonaBot', 'iconURL':'https://cdn.discordapp.com/app-icons/958405000101519372/2f4f565eb1a8418f0b95deb28776723b.png?size=512' })
-                    .setDescription('Laissez-moi vérifier en ' + depart_name + ` (${zipcode_depart}) Ville : ${cityInfo.nom}`)
-                    .addFields(
-                        { name: `Température actuelle dans le ${zipcode_depart}`, value : `:thermometer: ${weather.temp}°C ressenti ${weather.feels_temp} °C\r\n`},
-                        { name: `Météo actuelle dans le ${zipcode_depart}`, value : `Le temps est **${weather.desc}** ${emojiState}\r\n\r\n`})
-                    .setImage(photoURL)
-                    .addFields(
-                        { name: `Prévisions dans 6h dans le ${zipcode_depart}`, value : `:thermometer: ${forecast.temp}°C ressenti ${forecast.feels_temp} °C\r\n`},
-                        { name: `Prévisions dans 6h dans le ${zipcode_depart}`, value : `Le temps sera **${forecast.desc}** ${emojiStateForecast}`})
-                    .setTimestamp()
-                    .setFooter({ text: 'MonaBot météo fonctionne avec openweathermap.org et geo.api.gouv.fr', iconURL: 'https://openweathermap.org/themes/openweathermap/assets/img/mobile_app/android-app-top-banner.png' });
-                    
-                    message.reply({ embeds: [WeatherEmbed]});
-
-
-                    })
-                })
-            })  
-        })
-    }
-})
-
 client.on('interactionCreate', async interaction => {
 
-	const { commandName, componentType } = interaction;
+	const { commandName } = interaction;
 
 	if (commandName === 'mona') {
 		await interaction.reply({content:`Je suis là !`});
@@ -259,7 +145,6 @@ client.on('interactionCreate', async interaction => {
         } else {
             userCooldown[interaction.user.id] = Date.now()+60000;
         }
-        var temp = "pas de température trouvée..."
         var zipcode_depart = interaction.options.getString('zipcode');
         var depart_name = departements[zipcode_depart];
         if(zipcode_depart >= 971 && zipcode_depart <= 976) {   
@@ -278,7 +163,6 @@ client.on('interactionCreate', async interaction => {
             //get current weather
             getWeatherDataFrom(cityInfo.codesPostaux[0]).then(weather => {
                 var emojiState = ":question:";
-                var stateText;
                 if(weather.state == "Clouds") {
                     emojiState = ":cloud:";
                     weather.desc = "nuageux";
@@ -315,6 +199,7 @@ client.on('interactionCreate', async interaction => {
                         emojiStateForecast = ":cloud_rain:";
                         forecast.desc = "pluvieux";
                     }
+                    
                     getRandomFieldsPhoto()
                     .then(photoURL => {
                         WeatherEmbed = new MessageEmbed()
@@ -323,21 +208,21 @@ client.on('interactionCreate', async interaction => {
                         .setAuthor({ name: 'MonaBot', 'iconURL':'https://cdn.discordapp.com/app-icons/958405000101519372/2f4f565eb1a8418f0b95deb28776723b.png?size=512' })
                         .setDescription('Laissez-moi vérifier en ' + depart_name + ` (${zipcode_depart}) Ville : ${cityInfo.nom}`)
                         .addFields(
-                            { name: `Température actuelle dans le ${zipcode_depart}`, value : `:thermometer: ${weather.temp}°C ressenti ${weather.feels_temp} °C\r\n`},
-                            { name: `Météo actuelle dans le ${zipcode_depart}`, value : `Le temps est **${weather.desc}** ${emojiState}\r\n\r\n`})
+                            { name: `Météo actuelle dans le ${zipcode_depart}`, value : `:thermometer: ${weather.temp}°C ressenti ${weather.feels_temp} °C\r\n`},
+                            { name: `\u200b`, value : `Le temps est **${weather.desc}** ${emojiState}\r\n`, inline: true},
+                            { name: `\u200b`, value : `L'humidité est de **${weather.humidity}%**\r\n`, inline: true})
                         .setImage(photoURL)
                         .addFields(
                             { name: `Prévisions dans 6h dans le ${zipcode_depart}`, value : `:thermometer: ${forecast.temp}°C ressenti ${forecast.feels_temp} °C\r\n`},
-                            { name: `Prévisions dans 6h dans le ${zipcode_depart}`, value : `Le temps sera **${forecast.desc}** ${emojiStateForecast}`})
+                            { name: `\u200b`, value : `Le temps sera **${forecast.desc}** ${emojiStateForecast}`, inline: true},
+                            { name: `\u200b`, value : `L'humidité sera de **${forecast.humidity}%**\r\n`, inline: true})
                         .setTimestamp()
                         .setFooter({ text: 'MonaBot météo fonctionne avec openweathermap.org et geo.api.gouv.fr', iconURL: 'https://openweathermap.org/themes/openweathermap/assets/img/mobile_app/android-app-top-banner.png' });
-                        
                         interaction.reply({ embeds: [WeatherEmbed]});
                     })
-                   
+                    
                 })
             })
-              
         })
 
 	} else if (commandName === 'pain') {
@@ -347,8 +232,7 @@ client.on('interactionCreate', async interaction => {
         }
         
 
-	}
-    else if (commandName === 'actus') {
+	} else if (commandName === 'actus') {
 
         var actusEmbed = new MessageEmbed()
         .setColor('#0099ff')
@@ -367,8 +251,7 @@ client.on('interactionCreate', async interaction => {
       
         interaction.reply({ embeds: [actusEmbed]});
    
-    }
-    else if (commandName === 'journalmatinal') {
+    } else if (commandName === 'journalmatinal') {
         var activatedJournal = new MessageEmbed()
         .setColor('#0099ff')
         .setTitle('Journal matinal Mona activé ! :newspaper:')
@@ -382,8 +265,7 @@ client.on('interactionCreate', async interaction => {
         userDMChannel.send({content:":sparkles: **Bonjour !** Le **journal matinal** arrivera chaque jour à **6h** !:sparkles:", embeds: [activatedJournal]}); //
         interaction.reply({content: "Je vous enverrais un message tout les matins à 6h (heure française)", ephemeral: true});
         addUserToNewspaperSubscribers(interaction.user);
-    }
-    if(commandName === 'testjournal') {
+    } else if(commandName === 'testjournal') {
         getArticles();
         subscribedUsers.users.forEach((user) => {
            sendJournalTo(user);
@@ -397,8 +279,11 @@ client.on('interactionCreate', async interaction => {
 // Login to Discord with your client's token
 client.login(token);
 
-
-
+/**
+ * Returns weather information for a given zipcode
+ * @param {*} depart_code zip code
+ * @returns 
+ */
 async function getWeatherDataFrom(depart_code) {
     var weatherInDepartement = {
         city:"",
@@ -406,6 +291,7 @@ async function getWeatherDataFrom(depart_code) {
         state:"",
         windspeed:"",
         feels_temp:"",
+        humidity:"",
         desc:""
     };
 
@@ -418,6 +304,7 @@ async function getWeatherDataFrom(depart_code) {
             weatherInDepartement.city = json.name;
             weatherInDepartement.temp = json.main.temp;
             weatherInDepartement.feels_temp = json.main.feels_like;
+            weatherInDepartement.humidity = json.main.humidity;
             weatherInDepartement.state = json.weather[0].main;
             weatherInDepartement.windspeed = json.wind.speed;
             weatherInDepartement.desc = json.weather[0].description;
@@ -428,13 +315,18 @@ async function getWeatherDataFrom(depart_code) {
     
 }
 
-
+/**
+ * Return the weather in 6 hours in the departement
+ * @param {*} depart_code zip code
+ * @returns 
+ */
 async function getForecastDataFrom(depart_code) {
     var forecastInDepartement = {
         temp:"",
         state:"",
         windspeed:"",
         feels_temp:"",
+        humidity:"",
         desc:""
     };
 
@@ -446,6 +338,7 @@ async function getForecastDataFrom(depart_code) {
         .then(json => {
             forecastInDepartement.temp = json.list[1].main.temp; //1 gets the weather in 6 hours
             forecastInDepartement.feels_temp = json.list[1].main.feels_like;
+            forecastInDepartement.humidity = json.list[1].main.humidity;
             forecastInDepartement.state = json.list[1].weather[0].main;
             forecastInDepartement.windspeed = json.list[1].wind.speed;
             forecastInDepartement.desc = json.list[1].weather[0].description;
@@ -466,7 +359,6 @@ async function getBiggestCityOnDepartment(depart_code) {
         .then(json => {
             //extract the most populated one (could be ressource intensive for now.)
             var biggestCityPop = 0;
-            var biggestCity = "";
             var biggestCityJSON ;
             for (let i = 0; i < json.length; i++) {
                 const city = json[i];
@@ -514,7 +406,7 @@ function readAllSubscribedUsers() {
 async function sendJournalTo(userID) {
     console.log("getting journal ready");
     console.log(currenciesValues);
-    console.log(cryptoValues);
+    
     let foundUser = await client.users.fetch(userID);
     let userDMChannel = await foundUser.createDM();
     let newsInJournal = [];
@@ -522,8 +414,7 @@ async function sendJournalTo(userID) {
     console.log("creating the embed");
     let stringCrypto = "";
     cryptoValues.forEach((crypto) => {
-        console.log(crypto);
-        stringCrypto += `**${crypto.NAME}** \r **Prix** = ${crypto.PRICE}$ \r ${crypto.PERCENTAGE}% \r Haut = ${crypto.HIGH} \r Bas = ${crypto.LOW}\r\n`
+        stringCrypto += `**${crypto.NAME}** \r **Prix** = ${crypto.PRICE}$ \r ${crypto.PERCENTAGE}% \r Haut = ${crypto.HIGH}$ \r Bas = ${crypto.LOW}$\r\n`
     })
     console.log(stringCrypto);
     var actusEmbed = new MessageEmbed()
